1bd5eb0694ae56622cd6254c70beec906dc9782b
hiiam-andy <andrew.citb@gmail.com>
2023-07-20 20:00:44 +0300

    подключил аксиос, вёрстка хиро и карточек

---
+++ src/components/Hero.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="hero">
+    <h2 class="hero_header">Рестораны близко</h2>
+    <div class="">
+
+
+      <div class="hero_search">
+        <input class="hero_input">
+        <button class="hero_button">
+          <img :src="SEARCH" alt="search" class="search">
+        </button>
+        <button class="random">Выбрать случайный ресторан</button>
+      </div>
+    
+
+    </div>
+  </div>
+</template>
+
+<script>
+import SEARCH from '../store/search.svg'
+  export default {
+    data: function () {
+            return {
+                SEARCH: SEARCH
+            }
+        }
+  }
+</script>
+
+<style scoped>
+.hero{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 96px;
+  line-height: 104px;
+  color:#fff;
+  height: 500px;
+  background-color: rgb(142, 66, 219);
+  margin-bottom: 10px;
+}
+.hero_header{
+margin-bottom: 30px;
+}
+.hero_search{
+  display: flex;
+  height: 45px;
+}
+.hero_input{
+  height: 100%;
+  min-width: 500px;
+  width:20%;
+  flex: 1 1;
+  padding: 0 15px;
+  border: none;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  font-size: 16px;
+  color: #353b43;
+}
+.hero_input:focus{
+  outline: none;
+}
+.hero_button{
+  height: 100%;
+  width:50px;
+  background-color: #73b300;
+  border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+  border: none;
+  color:#fff;
+cursor: pointer;
+}
+.search{
+width: 25px;
+height: 25px;
+fill:#fff;
+}
+.random{
+  padding: 10px 12px;
+  margin-left: 15px;
+  color:#fff;
+  border: none;
+  border-radius: 4px;
+  background-color: #73b300;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
--- src/components/PlaceCard.vue
+++ src/components/PlaceCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="place" >
-    <h1>{{ place.name }}</h1>
-    <h3>{{ place.landmark }}</h3>
-    <p> средний чек: {{ place.price }}</p>
-    <p>{{ place.cuisine }}</p>
-    <img :src='place.photo' alt="img">
+  <div class="card" >
+    <img class="card_image" :src='place.photo' alt="img">
+    <div class="card_content">
+      <h1 class="card_header">{{ place.name }}</h1>
+      <h3>{{ place.landmark }}</h3>
+      <p> средний чек: {{ place.price }}</p>
+      <button >подробнее</button>
+    </div>
   </div>
 </template>
 
@@ -20,9 +22,27 @@
 </script>
 
 <style scoped>
-.place{
+.card{
+  border-radius: 15px;
+  box-shadow: 0 0 24px rgba(49,49,49,.28);
+  overflow: hidden;
+  min-width:200px;
+  max-width: 600px;
+  max-height: 370px;
+}
+.card_image{
+  width: 100%;
+  height:50%;
+}
+.card_content{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding: 15px;
-  border: 1px solid grey;
-  margin-bottom: 5px;
+  height: 50%;
+}
+.card_header{
+  font-size: 26px;
+    line-height: 28px;
 }
 </style>
