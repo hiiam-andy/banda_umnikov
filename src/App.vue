7d0f0cc551ca09862bb3c657cb0f1188a6cba5e4
hiiam-andy <andrew.citb@gmail.com>
2023-07-23 18:02:25 +0300

    убрал синий эффект тапа на мобилке

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,10 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+
   font-family: proxima,Helvetica Neue,Arial,sans-serif;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  -webkit-tap-highlight-color: transparent;
 }
 img {
   max-width: 100%;
